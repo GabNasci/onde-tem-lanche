a540dae7589bfc5141bf2d144c6475c81196700e
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2023-08-27 21:53:06 -0300

    Codando a página do restaurante e fazendo alguns ajustes

--- js/index.js
+++ js/index.js
@@ -11,6 +11,8 @@ async function initMap() {
         disableDefaultUI: true,
     });
 
+    
+    /*
     // Add a style-selector control to the map.
     const styleControl = document.getElementById("style-selector-control");
 
@@ -24,6 +26,55 @@ async function initMap() {
     styleSelector.addEventListener("change", () => {
       map.setOptions({ styles: styles[styleSelector.value] });
     });
+
+
+    const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
+  });
+
+  
+
+    let markerOptions = {
+        position: new google.maps.LatLng(-24.04800947308364, -52.380493073978386),
+    }
+
+    let marker = new google.maps.Marker(markerOptions);
+
+    marker.setIcon('./img/SVG/pin_marker.png');
+
+    marker.setMap(map);
+
+    /*marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+      });*/
+
+    
 }
 
 const styles = {
